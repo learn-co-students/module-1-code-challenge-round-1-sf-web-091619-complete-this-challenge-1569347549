b509693fbc79e93a65add0ed48bd81f9fc354b90
kat-star <katherinewkwong@gmail.com>
2019-09-24 12:21:48 -0700

    katherine's first test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,43 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+  
+  def reviews #helper for all reviews for Customer instance
+    Review.all.select { |review| review.customer == self }
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  def restaurants
+    reviews.map { |review| review.restaurant }.uniq
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |customer| customer.full_name == name }
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select { |customer| customer.first_name == name }
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,23 @@
 class Review
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content
+  @@all = []
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    if rating.between?(0, 5)
+      @rating = rating
+    else
+      @rating = nil
+    end
+    @content = content
+    @@all << self
+  end 
   
+  def self.all 
+    @@all
+  end
+
 end
 
