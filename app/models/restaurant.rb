4bf5bc2126d9c0d5ca82b5b19ac40f5add2f8ec8
github username <github email address>
2019-09-24 12:20:14 -0700

    code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| }
+  end
+
+  def customers
+    reviews.map {|review| review.customer.first_name}.uniq
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def average_start_rating
+    arr_ratings = reviews.map {|review| review.rating}
+    arr_ratings.inject { |sum, el| sum + el}.to_f/arr_ratings.size
+  end
+
+  def longest_review
+    reviews.max_by {|review| review.content}
+  end
+
+  def self.find_by_name(name)
+    @@all.select {|restaurant| restaurant.name == name}.first
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,23 @@
 class Review
-  
+    attr_reader :customer, :restaurant
+    attr_accessor :content, :rating
+    @@all = []
+
+    def initialize(customer, restaurant, content, rating) 
+        @customer = customer
+        @restaurant = restaurant
+        @content = content
+        if rating > 0 && rating < 6
+            @rating = rating
+        else
+            @rating = 0
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
+
 end
 
