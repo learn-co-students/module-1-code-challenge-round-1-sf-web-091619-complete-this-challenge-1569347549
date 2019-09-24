55210c0a1c220f8d65294862d4d11aa40ac63765
Mustafa Muse <umpp101@icloud.com>
2019-09-24 12:35:18 -0700

    message

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,59 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self,restaurant,content,rating)
+  end
+
+  #help method
+  def total_reviews
+    Review.all.select do |reviews|
+      reviews.customer == self
+    end
+  end
+
+  def num_reviews
+    total_reviews.size
+  end
+
+  def restaurants 
+    @@all.map do |reviews|
+
+    end
+  end
+
+
+  def self.find_by_name(full_name)
+    @@all.find do |el|
+      el.full_name == full_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.find do |el|
+      el.full_name == name
+    end 
+  end
+
+  def self.all_names
+    @@all.map do |el|
+      el.full_name
+    end
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,28 @@
 require 'bundler/setup'
 Bundler.require
 require_rel '../app'
+
+# first_customer = Customer.new("mustafa","muse")
+# second_customer = Customer.new("timothy","smith")
+# fastfood = Restaurant.new("mcdonalds")
+# fastfood = Restaurant.new("tacobell")
+# first = Review.new(muse,fastfood,"food", 5)
+# second = Review.new(tim,fastfood,"food", 9)
+
+# puts second
+
+first_customer = Customer.new("Mustafa", "Muse")
+second_customer = Customer.new("Will", "Smith")
+
+
+mcdonalds = Restaurant.new("Mcds")
+tacobell = Restaurant.new("Taco Bell!")
+
+
+review_1 = Review.new("muse",mcdonalds,"fastfood", 3 )
+review_2 = Review.new("tim",tacobell,"fastfood", 2 )
+
+
+binding.pry
+
+
