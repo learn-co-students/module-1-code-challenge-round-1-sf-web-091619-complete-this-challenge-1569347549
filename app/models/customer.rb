106b0c5fd5a70f2809e180676f8b71fd73052ccf
github username <github email address>
2019-09-24 12:06:24 -0700

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,60 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def rev
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def num_reviews
+    rev.count
+  end
+
+  def restaurant
+    rev.map do |review|
+      review.restaurant
+    end.uniq
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |c|
+      c.full_name == name
+    end
+  end
+
+  def self.all_names
+    @@all.map do |c|
+      c.full_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.map do |c|
+      c.full_name.include?(name)
+    end
+    
+  end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,50 @@
 class Restaurant
+
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customers
+    reviews.map do |review|
+      review.customer
+    end.uniq
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    total = reviews.select do |review|
+      review.rating
+    end.count
+    sum = reviews.sum do |review|
+      review.rating
+    end
+    sum / total
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |r|
+      r.name == name
+    end
+  end
+
+  def longest_review
+    reviews.max_by do |rev|
+      rev.content
+    end
   end
 
 end
